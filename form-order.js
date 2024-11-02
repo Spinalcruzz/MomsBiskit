2de08387d96edcfe202b6951dee153e94eee1063
Spinalcruzz <mikewebsite24@gmail.com>
2024-11-02 13:12:21 +0200

    Add files via upload

--- form-order.js
+++ form-order.js
@@ -3,33 +3,39 @@ let clientInfo = []
 
 function submitBtn() {
  
-    const batchAmount = document.querySelector('.batchAmount');
+    const batchAmount = document.querySelector('#batchAmount');
     const amount = batchAmount.value; 
             
         
     clientInfo.push(amount);
      
-     const productDescription = document.querySelector('.productDescription');
+     const productDescription = document.querySelector('#productDescription');
      const description = productDescription.value;
 
 
     clientInfo.push(description);
       
-    const clientContact = document.querySelector('.clientContact');
+    const clientContact = document.querySelector('#clientContact');
     const contact = clientContact.value;
 
     clientInfo.push(contact);
 
-    const clientEmail = document.querySelector('.clientEmail');
+    const clientEmail = document.querySelector('#clientEmail');
     const email = clientEmail.value;
 
     clientInfo.push(email);
     
-    if ( amount || description || contact || email != "" ) {
+    FormCompletion(amount, description, contact, email);
+    
+    
+};
+
+//document.querySelector('.clientOrderInfo').innerHTML = `Please fill in the Form!`
+
+function FormCompletion(totalAmnt, describe, contct, emailAddrss) {
+    if ( totalAmnt || describe || contct || emailAddrss !== "" ) {
         const clientOrderInfo = document.querySelector('.clientOrderInfo').innerHTML =`Batch Amount ${clientInfo[0]} , Product Description of ${clientInfo[1]} ,we will contact ${clientInfo[2]} and Send an email to ${clientInfo[3]}`;
     } else {
-         document.querySelector('.clientOrderInfo').innerHTML = `Please fill in the Form!`
-    }
-   
-}
-
+        document.querySelector('.clientOrderInfo').innerHTML = `Please fill in the Form!`;
+    };
+};
